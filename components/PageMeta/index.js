000a9e36db1bca5dbfd5ca1f870d57ca3a02da8b
Joey Hage <joeyhage@outlook.com>
2021-11-14 20:18:17 -0600

    change theme color

--- components/PageMeta/index.js
+++ components/PageMeta/index.js
@@ -44,7 +44,7 @@ export default function PageMeta(props) {
       <link rel="manifest" href="/site.webmanifest" />
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#0f111a" />
       <meta name="msapplication-TileColor" content="#b91d47" />
-      <meta name="theme-color" content="#f11012" />
+      <meta name="theme-color" content="#283848" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
       <link
