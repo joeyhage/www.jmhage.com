850b495bed2cf3d0543fbcda5fa14ea182c95d1e
Joey Hage <joeyhage@outlook.com>
2021-11-17 10:28:23 -0600

    add tags to project

--- components/Project/index.js
+++ components/Project/index.js
@@ -1,12 +1,14 @@
 import ExternalUrl from "@components/Project/ExternalUrl";
+import Tags from "@components/Project/Tags";
 import RichTextPageContent from "@components/RichTextPageContent";
 import RichTextPageContentStyles from "@styles/RichTextPageContent.module.css";
 import TypographyStyles from "@styles/Typography.module.css";
 import Image from "next/image";
 
-export default function Project({ project }) {
+export default function Project({ project, topics }) {
   return (
     <article className={RichTextPageContentStyles.page}>
+      {!!topics && <Tags tags={topics} />}
       <h1 className={TypographyStyles.heading__h1}>{project.title}</h1>
       <figure className="image is-16by9 mb-5">
         <Image
