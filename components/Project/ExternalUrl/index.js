883c55851e746f5389b3be874d94fce153b4a663
Joey Hage <joeyhage@outlook.com>
2021-11-17 10:04:24 -0600

    make externalurl dynamic

--- components/Project/ExternalUrl/index.js
+++ components/Project/ExternalUrl/index.js
@@ -1,14 +1,14 @@
 import ExternalUrlStyles from "@styles/ExternalUrl.module.css";
 import Link from "next/link";
 
-export default function ExternalUrl({ url }) {
+export default function ExternalUrl({ children, url }) {
   function formatUrlForDisplay(url) {
     return new URL(url).hostname;
   }
 
   return (
     <div>
-      <h2 className="title is-4">Project link</h2>
+      <h2 className="title is-4">{children}</h2>
       <p className={ExternalUrlStyles.externalUrl__text}>
         <Link href={url}>
           <a
